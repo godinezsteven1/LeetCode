166d81d342ecdbc4dd70917c69c37f8f69da22b2
Steven Godinez <steven.godin.42@gmail.com>
2026-09-05 15:15:41 -0400

    [56] [Time Beats: 21.54%] [Memory Beats: 26.75%] - LeetPush

--- Medium/Java/56-Merge-Intervals.java
+++ Medium/Java/56-Merge-Intervals.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
         List<int[]> answer = new ArrayList<>();
         int[] curr = intervals[0];
-        
-        for (int i = 1; i < intervals.length; i++) {
+
+        for (int i = 0; i < intervals.length; i++) {
             int[] next = intervals[i];
             if (curr[1] >= next[0]) {
                 curr[1] = Math.max(curr[1], next[1]);
@@ -15,6 +15,5 @@ class Solution {
         }
         answer.add(curr);
         return answer.toArray(new int[answer.size()][]);
-
     }
 }
