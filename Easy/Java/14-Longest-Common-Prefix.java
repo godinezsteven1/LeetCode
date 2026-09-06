da95fb36d964b279123e0897a7f867c0ef62443f
Steven Godinez <steven.godin.42@gmail.com>
2026-09-05 20:55:56 -0400

    [14] [Time Beats: 12.06%] [Memory Beats: 7.65%] - LeetPush

---
+++ Easy/Java/14-Longest-Common-Prefix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String answer = "";
+        String first = strs[0];
+
+        for(int i = 0; i < first.length(); i++) {
+            char curr = first.charAt(i);
+
+            for (int n = 1; n < strs.length; n++) {
+                if (i >= strs[n].length()) {
+                    return answer;
+                }
+                char curr2 = strs[n].charAt(i);
+                if (curr != curr2) {
+                    return answer;
+                }
+                
+            } 
+            answer += curr;
+        }
+
+
+        return answer;
+    }
+}
