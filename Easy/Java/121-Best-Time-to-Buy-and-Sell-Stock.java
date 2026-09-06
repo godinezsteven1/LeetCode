eea7924deaa7a5f738ab3db230754b8f39999834
Steven Godinez <steven.godin.42@gmail.com>
2026-09-06 12:08:24 -0400

    [121] [Time Beats: 99.95%] [Memory Beats: 15.59%] - LeetPush

--- Easy/Java/121-Best-Time-to-Buy-and-Sell-Stock.java
+++ Easy/Java/121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,9 +1,11 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int left = 0;
+    public int maxProfit(int[] prices) { 
+        // base cases; no profit, prices empty, 
+        // constraints? 
         int max = 0;
+        int left = 0;
         for (int right = 0; right < prices.length; right++) {
-            if (prices[left] > prices[right]) { // ,1,2
+            if (prices[left] > prices[right]) {
                 left = right;
             }
             int profit = prices[right] - prices[left];
