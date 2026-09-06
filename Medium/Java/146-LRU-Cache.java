b2ea7747ceab60a4a40faa8f9652ba07d5c46966
Steven Godinez <steven.godin.42@gmail.com>
2026-09-06 10:48:44 -0400

    [146] [Time Beats: 96.74%] [Memory Beats: 74.27%] - LeetPush

--- Medium/Java/146-LRU-Cache.java
+++ Medium/Java/146-LRU-Cache.java
@@ -1,63 +1,65 @@
 class LRUCache {
 
-    private int capacity; 
+    private int capacity;
     private HashMap<Integer, LinkNode> map;
+    private int key; 
+    private int val;
+    private LinkNode headLRU; 
     private LinkNode tailMRU;
-    private LinkNode headLRU;
-
-
 
     private class LinkNode {
         private int key;
-        private int val;
+        private int val; 
         private LinkNode prev;
         private LinkNode next;
 
-        private LinkNode(int key, int val) {
-            this.key = key; 
+        LinkNode(int key, int val) {
+            this.key = key;
             this.val = val;
         }
     }
 
-    public LRUCache(int capacity) { 
-        this.capacity = capacity; 
-        this.map = new HashMap<>(capacity); 
+    public LRUCache(int capacity) {
+        this.map = new HashMap<>(capacity);
+        this.capacity = capacity;
+        this.key = key; 
+        this.val = val;
+        this.headLRU = new LinkNode(0,0);
         this.tailMRU = new LinkNode(0,0);
-        this.headLRU = new LinkNode(0,0); 
-        tailMRU.prev = headLRU;
         headLRU.next = tailMRU;
+        tailMRU.prev = headLRU;
     }
     
-    public int get(int key) { 
+    public int get(int key) {
         if (!map.containsKey(key)) {
             return -1;
         }
-        LinkNode dummy = map.get(key);
-        updateCache(dummy);
+        LinkNode dummy = map.get(key); 
+        updateCache(dummy);  
         return dummy.val;
+        
     }
     
     public void put(int key, int value) {
-
         if (map.containsKey(key)) {
-            LinkNode dummy = map.get(key);
+            LinkNode dummy = map.get(key); 
             dummy.val = value;
             updateCache(dummy);
             return;
-        } else
-        if (map.size() == capacity) { 
-            LinkNode lru = headLRU.next;
+        }
+        if (map.size() == capacity) {
+            LinkNode lru = headLRU.next; // [head:fake] <-> [real] <-> [][][][[]]
             remove(lru);
             map.remove(lru.key);
         }
         LinkNode dummy = new LinkNode(key, value);
-        map.put(key, dummy);
         insertMRU(dummy);
+        map.put(key, dummy); 
     }
 
     private void remove(LinkNode dummy) {
-        dummy.prev.next = dummy.next;
-        dummy.next.prev = dummy.prev; 
+        dummy.prev.next = dummy.next; 
+        dummy.next.prev = dummy.prev;
     }
 
     private void insertMRU(LinkNode dummy) {
