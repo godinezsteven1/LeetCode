15ab1b52dea42aeedda2336dfc0dd704e799137a
Steven Godinez <steven.godin.42@gmail.com>
2026-09-06 11:14:19 -0400

    [49] [Time Beats: 99.20%] [Memory Beats: 95.93%] - LeetPush

--- Medium/Java/49-Group-Anagrams.java
+++ Medium/Java/49-Group-Anagrams.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
-        for (String word: strs) {
-            char[] c = word.toCharArray();
-            Arrays.sort(c);
-            String sorted = new String(c);
+        for(String string: strs) {
+            char[] charV = string.toCharArray();
+            Arrays.sort(charV);
+            String sorted = new String(charV);
             if (!map.containsKey(sorted)) {
                 map.put(sorted, new ArrayList<>());
-                
             }
-            map.get(sorted).add(word);
+            map.get(sorted).add(string);
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList<>(map.values()); 
     }
 }
