5f7f2842fcbbe5ee4e249bf3cd87927adb84abe2
Steven Godinez <steven.godin.42@gmail.com>
2026-09-07 23:16:49 -0400

    [198] [Time Beats: 100.00%] [Memory Beats: 56.52%] - LeetPush

--- Medium/Java/198-House-Robber.java
+++ Medium/Java/198-House-Robber.java
@@ -1,23 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        // base cases
-        if (nums.length == 0) { 
-            return 0;
-        }
-        if (nums.length == 1) {
+
+        if (nums.length < 2) {
             return nums[0];
         }
-        // init table
+
         int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        // memoization pattern
         
-        for(int n = 2; n < nums.length; n++) {
-            // transition state
-            dp[n] = Math.max(
-                dp[n - 1], // skip n 
-                dp[n - 2] + nums[n]); // take n 
+        // set base case
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        // now we have our first case of n-2 and n-1 
+
+        for(int n = 2; n < dp.length; n++) {
+            dp[n] = Math.max(dp[n - 2] + nums[n], dp[n - 1]);
         }
+
         return dp[nums.length - 1];
+        
     }
 }
