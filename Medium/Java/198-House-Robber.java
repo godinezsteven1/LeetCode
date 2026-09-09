676351783191ded89ccc0981d3231168af2fd32f
Steven Godinez <steven.godin.42@gmail.com>
2026-09-09 12:49:12 -0400

    [198] [Time Beats: 100.00%] [Memory Beats: 19.34%] - LeetPush

--- Medium/Java/198-House-Robber.java
+++ Medium/Java/198-House-Robber.java
@@ -6,10 +6,11 @@ class Solution {
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(dp[0], nums[1]);
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        for(int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-        return dp[nums.length - 1];
 
+        return dp[nums.length - 1];
+        
     }
 }
