aafd9638a3f1216815283e9cc7294d2ea3caf953
Steven Godinez <steven.godin.42@gmail.com>
2026-09-09 12:46:33 -0400

    [1049] [Time Beats: 31.36%] [Memory Beats: 83.70%] - LeetPush

--- Medium/Java/1049-Last-Stone-Weight-II.java
+++ Medium/Java/1049-Last-Stone-Weight-II.java
@@ -1,21 +1,14 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-        // naive brute force
-        // DP + 0/1 knapsack 
-
-        // [.      ] 
-        //sum == 23   
-        //   12     11
-
         int sum = Arrays.stream(stones).sum();
         int target = sum / 2;
         int[] dp = new int[target + 1];
+
         for(int stone: stones) {
             for(int i = target; i >= stone; i--) {
                 dp[i] = Math.max(dp[i], dp[i - stone] + stone);
             }
         }
         return sum - 2 * dp[target];
-
     }
 }
