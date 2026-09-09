5dab2382022f6a9cc64c659dcf72ab9467f589cc
Steven Godinez <steven.godin.42@gmail.com>
2026-09-09 01:41:34 -0400

    [1046] [Time Beats: 99.67%] [Memory Beats: 51.32%] - LeetPush

--- Easy/Java/1046-Last-Stone-Weight.java
+++ Easy/Java/1046-Last-Stone-Weight.java
@@ -1,19 +1,20 @@
 class Solution {
-
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); //min
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0; i < stones.length; i++) {
             maxHeap.add(stones[i]);
         }
+
         while (maxHeap.size() > 1) {
-            int first = maxHeap.poll(); // largest 
-            int second = maxHeap.poll(); // second 
-            if (second > first) {
-                maxHeap.add(second - first);
-            } else {
+            int first = maxHeap.poll();
+            int second = maxHeap.poll();
+            if (first > second) {
                 maxHeap.add(first - second);
+            } else {
+                maxHeap.add(second - first);
             }
         }
         return maxHeap.poll();
+        
     }
 }
