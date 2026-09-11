390a725aa13c494d547fdaa049425f62504aae05
Steven Godinez <steven.godin.42@gmail.com>
2026-09-11 11:32:39 -0400

    [1146] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- Medium/Java/1146-Snapshot-Array.java
+++ Medium/Java/1146-Snapshot-Array.java
@@ -1,27 +1,31 @@
 class SnapshotArray {
 
+    ArrayList<Pair>[] history;
+    int snapID;
+    int length;
 
-    private ArrayList<Pair>[] history;
-    private int snapID;
-    private int length; 
-
-    
     private class Pair {
-        private int pastSnapID;
-        private int val;
-
-        Pair(int pastSnapID, int val ) {
-            this.pastSnapID = pastSnapID;
+        int pastSnapID; 
+        int val;
+        Pair(int pastSnapID, int val) {
+            this.pastSnapID = pastSnapID; 
             this.val = val;
         }
     }
 
+    // ArrayList<Pair>[] history
+
+    // [ [(snapshot, 4)], [] ,[]   ] 
+
     public SnapshotArray(int length) {
         this.history = new ArrayList[length];
         this.snapID = 0;
+        this.length = length; 
+
         for(int i = 0; i < length; i++) {
             history[i] = new ArrayList<>();
         }
+        
     }
     
     public void set(int index, int val) {
@@ -29,7 +33,7 @@ class SnapshotArray {
     }
     
     public int snap() {
-        return snapID++; 
+        return snapID++;
     }
     
     public int get(int index, int snap_id) {
@@ -37,15 +41,15 @@ class SnapshotArray {
         int left = 0;
         int right = list.size() - 1;
         int answer = 0;
-
-        while (left <= right) { 
-            int mid = left + (right - left) / 2; 
+        while (left <= right) {
+            int mid = (left + right) / 2;
             Pair curr = list.get(mid);
             if (curr.pastSnapID <= snap_id) {
-                answer = curr.val;
+                // 6 <= 6 _ _ _ _ _ _ 
                 left = mid + 1;
+                answer = curr.val;
             } else 
-            if (curr.pastSnapID >= snap_id) {
+            if (curr.pastSnapID > snap_id) {
                 right = mid - 1;
             }
         }
