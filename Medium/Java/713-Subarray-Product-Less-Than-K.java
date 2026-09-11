fadc714cca35e3e6c00b2fe336d2436ebfd411a5
Steven Godinez <steven.godin.42@gmail.com>
2026-09-10 23:19:11 -0400

    [713] [Time Beats: 99.95%] [Memory Beats: 65.41%] - LeetPush

--- Medium/Java/713-Subarray-Product-Less-Than-K.java
+++ Medium/Java/713-Subarray-Product-Less-Than-K.java
@@ -1,20 +1,19 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if (k <= 1) {
-            return 0; 
+            return 0;
         }
-
-        int left = 0;
-        int product = 1; // (Z,*)
-        int count = 0;
-        for (int right = 0; right < nums.length; right++) {
-            product *= nums[right];
-            while (product >= k) {
-                product /= nums[left];
-                left++;
-            }
-            count += right - left + 1;
+    int left = 0;
+    int count = 0;
+    int product = 1;
+    for(int right = 0; right < nums.length; right++) {
+        product *= nums[right];
+        while (product >= k) {
+            product /= nums[left];
+            left++;
         }
-        return count;
+        count += right - left + 1;
+    }
+    return count; 
     }
 }
