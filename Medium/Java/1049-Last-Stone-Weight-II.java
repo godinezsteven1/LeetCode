e2db7f11923bcbd4ef5bc1c5cb40f69643540142
Steven Godinez <steven.godin.42@gmail.com>
2026-09-12 10:35:50 -0400

    [1049] [Time Beats: 31.09%] [Memory Beats: 75.60%] - LeetPush

--- Medium/Java/1049-Last-Stone-Weight-II.java
+++ Medium/Java/1049-Last-Stone-Weight-II.java
@@ -1,31 +1,11 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-
-
-        /**
-        
-        sum stones = 23  int java / 2
-                    /  \
-                   11.  12 = 1
-                   23 - 22 = 1
-        
-        stones sum 
-        int target
-        dp[target]
-
-        for stone in stones 
-            for i = target; i >= stone; i--
-                dp[i] = max(dp[i], dp[i - stone] + stone)
-         */
-        
-        // return 2 * dp[target]
-
         int sum = Arrays.stream(stones).sum();
         int target = sum / 2;
         int[] dp = new int[target + 1];
 
         for(int stone: stones) {
-            for (int i = target; i >= stone; i--) {
+            for(int i = target; i >= stone; i--) {
                 dp[i] = Math.max(dp[i], dp[i - stone] + stone);
             }
         }
