b9d5053e285bd98cb3b142cb661b1c7541529ef0
Steven Godinez <steven.godin.42@gmail.com>
2026-09-13 23:46:13 -0400

    [670] [Time Beats: 100.00%] [Memory Beats: 67.23%] - LeetPush

--- Medium/Java/670-Maximum-Swap.java
+++ Medium/Java/670-Maximum-Swap.java
@@ -2,26 +2,27 @@ class Solution {
     public int maximumSwap(int num) {
         int max = -1;
         int max_i = -1;
-        int swapIndex = -1;
-        int swapWithIndex = -1; 
-        char[] digits = Integer.toString(num).toCharArray();
+        int swapFrom = -1;
+        int swapWithValue = -1;
 
+        char[] digits = Integer.toString(num).toCharArray();
         for(int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] > max) {
                 max = digits[i];
                 max_i = i;
             }
             if (digits[i] < max) {
-                swapIndex = i;
-                swapWithIndex = max_i;
+                swapFrom = i;
+                swapWithValue = max_i;
             }
         }
-        if (swapIndex != -1) {
-            char temp = digits[swapIndex];
-            digits[swapIndex] = digits[swapWithIndex];
-            digits[swapWithIndex] = temp;
+        
+        if (swapFrom != -1) {
+            char temp = digits[swapFrom];
+            digits[swapFrom] = digits[swapWithValue];
+            digits[swapWithValue] = temp;
         }
 
-        return Integer.parseInt(new String(digits));        
+        return Integer.parseInt(new String(digits));
     }
 }
