3dc0ce624293d23ffa7e2bd6be2f54f4f874c11e
Steven Godinez <steven.godin.42@gmail.com>
2026-09-13 22:52:03 -0400

    [200] [Time Beats: 46.01%] [Memory Beats: 88.86%] - LeetPush

--- Medium/Java/200-Number-of-Islands.java
+++ Medium/Java/200-Number-of-Islands.java
@@ -1,64 +1,54 @@
-import java.util.Queue;
-import java.util.LinkedList;
-
 class Solution {
 
-    static class Pair {
+    private class Pair {
         int x;
         int y;
-
-        public Pair(int x, int y) {
+        Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
     public int numIslands(char[][] grid) {
-        // horizontal vertical connections are islands. 
         int islandCounter = 0;
-
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[0].length; col++) {
                 if (grid[row][col] == '1') {
                     islandCounter++;
-                    bfs(grid, row, col);
+                    bfs(row, col, grid);
                 }
             }
         }
         return islandCounter;
     }
 
-    private void bfs(char[][] grid, int row, int col) {
-
+    private void bfs(int row, int col, char[][] grid) {
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(row, col));
         grid[row][col] = '0';
 
         while (!q.isEmpty()) {
-            Pair cell = q.remove();
-            int x = cell.x;
-            int y = cell.y;
-            checkNeighbors(grid, q, x, y);
+            Pair cell = q.poll();
+            checkNeighbors(cell.x, cell.y, q, grid);
         }
     }
 
-
-    private void checkNeighbors(char[][] grid, Queue<Pair> q, int x, int y) {
-        if (x - 1 >= 0 && grid[x - 1][y] == '1') {
-            grid[x - 1][y] = '0'; 
-            q.add(new Pair(x - 1, y));
-        }
-        if (x + 1 < grid.length && grid[x + 1][y] == '1') {
-            grid[x + 1][y] = '0';
-            q.add(new Pair(x + 1, y));
-        }
+    private void checkNeighbors(int x, int y, Queue<Pair> q, char[][] grid) {
         if (y - 1 >= 0 && grid[x][y - 1] == '1') {
             grid[x][y - 1] = '0';
             q.add(new Pair(x, y - 1));
         }
-        if (y + 1 < grid[0].length && grid[x][y + 1] == '1') {
+        if (y + 1 <= grid[0].length - 1 && grid[x][y + 1] == '1') {
             grid[x][y + 1] = '0';
             q.add(new Pair(x, y + 1));
+        }
+        if (x - 1 >= 0 && grid[x - 1][y] == '1') {
+            grid[x - 1][y] = '0';
+            q.add(new Pair(x - 1, y));
+        }
+        if (x + 1 <= grid.length - 1 && grid[x + 1][y] == '1') {
+            grid[x + 1][y] = '0';
+            q.add(new Pair(x + 1, y));
         }
     }
 }
