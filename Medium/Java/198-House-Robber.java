eda43ece2b197c856fa1ec258d3e211a64184576
Steven Godinez <steven.godin.42@gmail.com>
2026-09-19 11:58:23 -0400

    [198] [Time Beats: 100.00%] [Memory Beats: 94.21%] - LeetPush

--- Medium/Java/198-House-Robber.java
+++ Medium/Java/198-House-Robber.java
@@ -3,7 +3,8 @@ class Solution {
         if (nums.length < 2) {
             return nums[0];
         }
-        int[] dp = new int[nums.length + 1];
+  
+        int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(dp[0], nums[1]);
 
@@ -11,6 +12,6 @@ class Solution {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
 
-        return dp[nums.length - 1]; 
+        return dp[nums.length - 1];
     }
 }
