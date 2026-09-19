0158a6aeb5964a964253f88fe05fcafed64d49b6
Steven Godinez <steven.godin.42@gmail.com>
2026-09-19 13:02:47 -0400

    [200] [Time Beats: 5.24%] [Memory Beats: 88.97%] - LeetPush

--- Medium/Java/200-Number-of-Islands.java
+++ Medium/Java/200-Number-of-Islands.java
@@ -11,28 +11,28 @@ class Solution {
 
     public int numIslands(char[][] grid) {
         int islandCounter = 0;
-
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
+        
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[0].length; col++) {
                 if (grid[row][col] == '1') {
                     islandCounter++;
                     bfs(row, col, grid);
                 }
             }
         }
+
         return islandCounter;
     }
 
     private void bfs(int row, int col, char[][] grid) {
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(row, col));
         grid[row][col] = '0';
+        q.add(new Pair(row, col));
 
         while (!q.isEmpty()) {
             Pair cell = q.poll();
             checkNeighbors(cell.x, cell.y, q, grid);
         }
-
     }
 
     private void checkNeighbors(int x, int y, Queue<Pair> q, char[][] grid) {
@@ -40,7 +40,7 @@ class Solution {
             grid[x - 1][y] = '0';
             q.add(new Pair(x - 1, y));
         }
-        if (x + 1 <= grid.length - 1 && grid[x + 1][y] == '1') {
+        if (x + 1 < grid.length && grid[x + 1][y] == '1') {
             grid[x + 1][y] = '0';
             q.add(new Pair(x + 1, y));
         }
@@ -48,7 +48,7 @@ class Solution {
             grid[x][y - 1] = '0';
             q.add(new Pair(x, y - 1));
         }
-        if (y + 1 <= grid[0].length - 1 && grid[x][y + 1] == '1') {
+        if (y + 1 < grid[0].length && grid[x][y + 1] == '1') {
             grid[x][y + 1] = '0';
             q.add(new Pair(x, y + 1));
         }
