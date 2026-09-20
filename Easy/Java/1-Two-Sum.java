062beb5958633d942605917c697231a66209d908
Steven Godinez <steven.godin.42@gmail.com>
2026-09-20 16:51:06 -0400

    [1] [Time Beats: 99.39%] [Memory Beats: 11.43%] - LeetPush

--- Easy/Java/1-Two-Sum.java
+++ Easy/Java/1-Two-Sum.java
@@ -1,18 +1,18 @@
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int complement = 0;
-        int curr = 0;
-        for(int i = 0; i < nums.length; i++) { 
-            curr = nums[i];
-            complement = target - curr;
+
+        for(int i = 0; i < nums.length; i++) {
+            complement = target - nums[i];
             if (map.containsKey(complement) && 
             map.get(complement) != i) {
                 return new int[] {map.get(complement), i};
             }
-            map.put(curr, i);
+            map.put(nums[i], i);
         }
-        return new int[] {0,0};
-        
+
+        return new int[] {0,0};   
     }
 }
