a2d82ca6c50e8a8135854103a9f244a5c34a73ac
Steven Godinez <steven.godin.42@gmail.com>
2026-09-20 19:04:24 -0400

    [857] [Time Beats: 60.63%] [Memory Beats: 43.18%] - LeetPush

--- Hard/Java/857-Minimum-Cost-to-Hire-K-Workers.java
+++ Hard/Java/857-Minimum-Cost-to-Hire-K-Workers.java
@@ -1,48 +1,75 @@
 class Solution {
 
+
     private class Pair {
         double ratio; 
         int quality; 
-        Pair(int quality, double ratio) {
-            this.quality = quality; 
+        Pair(double ratio, int quality) {
             this.ratio = ratio;
+            this.quality = quality;
         }
     }
 
-
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
         
-        // worker must be paid min expectation 
-        // pay is proprotioinal to quality. 
-        // naive approach check double, 2x pay ex) 100=20  10 : 10 5 
-        // base cases: quality length != wage length 
-        // k <= 1
-        // constraints? 
 
-        // quality and wage 70:10 = 7.0  25:30 : 1.25? 
+        // answer expected in double 
+        /**
+
+        base cases: k < 1, quality length != wage length || empty .. 
+        
+        
+        naive: 
+        quality = [10,20,5], wage = [70,50,60], k = 2
+
+
+        optimal: 
+
+        ratio = wage / quality 
+        array(integer = ratio) add all of these values; 
+
+        sort ascending 
+        lowerst ratio --> ratio 
+        lowest            highest
+
+
+        maxHeap
+        qualitysum 
+        min 
+
+        for worker in our array
+            max heap . add worker quality 
+            sum += worker.quality 
+
+            heap size > k 
+                sum -= heap.poll()
+            heap == k 
+            cost = qualitysum * worker.ratio
+            min (cost, min)
+        min 
+         */
+
+
         ArrayList<Pair> wageQualityRatio = new ArrayList<>();
-        for(int i = 0; i < quality.length; i++) {
+        for(int i = 0; i < wage.length; i++) {
             double ratio = (double) wage[i] / quality[i];
-            wageQualityRatio.add(new Pair(quality[i], ratio));
+            wageQualityRatio.add(new Pair(ratio, quality[i]));
         }
-        wageQualityRatio.sort( (a,b) -> Double.compare(a.ratio, b.ratio));
-        // max heap of quality
+        wageQualityRatio.sort((a,b) -> Double.compare(a.ratio, b.ratio));
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        int qualitySum = 0; // low mid high 
+        int qualitySum = 0;
         double min = Double.MAX_VALUE;
         for(Pair worker: wageQualityRatio) {
             maxHeap.add(worker.quality);
-            qualitySum += worker.quality; 
-
+            qualitySum += worker.quality;
             if (maxHeap.size() > k) {
                 qualitySum -= maxHeap.poll();
             }
             if (maxHeap.size() == k) {
                 double cost = qualitySum * worker.ratio;
-                min = Math.min(min, cost);
+                min = Math.min(cost, min);
             }
         }
         return min;
-        
     }
 }
