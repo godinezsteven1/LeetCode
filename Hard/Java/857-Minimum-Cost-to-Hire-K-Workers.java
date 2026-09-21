799efdf70f5c5299a89ad27ea21a97e8db559e88
Steven Godinez <steven.godin.42@gmail.com>
2026-09-20 20:12:48 -0400

    [857] [Time Beats: 47.09%] [Memory Beats: 64.99%] - LeetPush

--- Hard/Java/857-Minimum-Cost-to-Hire-K-Workers.java
+++ Hard/Java/857-Minimum-Cost-to-Hire-K-Workers.java
@@ -1,57 +1,18 @@
 class Solution {
 
-
     private class Pair {
         double ratio; 
-        int quality; 
+        int quality;
         Pair(double ratio, int quality) {
             this.ratio = ratio;
             this.quality = quality;
         }
     }
 
+
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        
-
-        // answer expected in double 
-        /**
-
-        base cases: k < 1, quality length != wage length || empty .. 
-        
-        
-        naive: 
-        quality = [10,20,5], wage = [70,50,60], k = 2
-
-
-        optimal: 
-
-        ratio = wage / quality 
-        array(integer = ratio) add all of these values; 
-
-        sort ascending 
-        lowerst ratio --> ratio 
-        lowest            highest
-
-
-        maxHeap
-        qualitysum 
-        min 
-
-        for worker in our array
-            max heap . add worker quality 
-            sum += worker.quality 
-
-            heap size > k 
-                sum -= heap.poll()
-            heap == k 
-            cost = qualitysum * worker.ratio
-            min (cost, min)
-        min 
-         */
-
-
         ArrayList<Pair> wageQualityRatio = new ArrayList<>();
-        for(int i = 0; i < wage.length; i++) {
+        for(int i = 0; i < quality.length; i++) {
             double ratio = (double) wage[i] / quality[i];
             wageQualityRatio.add(new Pair(ratio, quality[i]));
         }
@@ -60,8 +21,8 @@ class Solution {
         int qualitySum = 0;
         double min = Double.MAX_VALUE;
         for(Pair worker: wageQualityRatio) {
-            maxHeap.add(worker.quality);
             qualitySum += worker.quality;
+            maxHeap.add(worker.quality);
             if (maxHeap.size() > k) {
                 qualitySum -= maxHeap.poll();
             }
