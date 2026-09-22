30bc3d451c6b82d4532f2211a730efabfca70067
Steven Godinez <steven.godin.42@gmail.com>
2026-09-21 23:53:22 -0400

    [125] [Time Beats: 28.19%] [Memory Beats: 22.61%] - LeetPush

--- Easy/Java/125-Valid-Palindrome.java
+++ Easy/Java/125-Valid-Palindrome.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "");
-        s = s.toLowerCase();
+        s = s.toLowerCase(); 
         int right = s.length() - 1;
-        int mid = s.length() / 2; 
-
-        for(int left = 0; left < mid; left++) {
+        int middle = right / 2; 
+        for(int left = 0; left < right; left++) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
             right--;
         }
         return true;
+        /**
+        constraints: 
+            upper case; 
+            numbers 
+        naive: 
+            cumbersome approach to constraints 
+
+        optimal :
+
+        two pointers coming from left to right and just charAt their index; 
+        remove upper case and non alpanumeric characters 
+        */
     }
 }
