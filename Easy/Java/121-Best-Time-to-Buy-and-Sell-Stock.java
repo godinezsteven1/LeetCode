e1e2bfe7480f7850850833f7fa868786ff4da725
Steven Godinez <steven.godin.42@gmail.com>
2026-09-24 23:23:21 -0400

    [121] [Time Beats: 99.94%] [Memory Beats: 84.87%] - LeetPush

--- Easy/Java/121-Best-Time-to-Buy-and-Sell-Stock.java
+++ Easy/Java/121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,27 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-
-        /**
-        
-        naive: 
-            brute force of am i making profit if i buy today and sell in future
-        
-        optimal sol: O(n)
-        left , right = for loop pointer
-
-        if (prices[right] < prices[left])
-            left = right;
-        profit? (init at 0)
-
-        
-        
-        */
-
-        int profit = 0;
         int left = 0;
+        int profit = 0; 
 
         for(int right = 0; right < prices.length; right++) {
-            if (prices[right] < prices[left]) {
+            if (prices[left] > prices[right]) {
                 left = right;
             }
             profit = Math.max(profit, prices[right] - prices[left]);
