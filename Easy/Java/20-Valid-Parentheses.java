702d5d6350641124c7505604990d8440db91845a
Steven Godinez <steven.godin.42@gmail.com>
2026-09-24 23:45:07 -0400

    [20] [Time Beats: 97.89%] [Memory Beats: 57.33%] - LeetPush

--- Easy/Java/20-Valid-Parentheses.java
+++ Easy/Java/20-Valid-Parentheses.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>(); 
+        
+        Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if (curr == '{' || curr == '(' || curr == '[') {
-                stack.push(s.charAt(i));
-            } else 
-            if (stack.isEmpty()) {
-                return false;
-            } else 
-            if (curr == '}' && stack.peek() == '{') {
-                stack.pop();
-            } else
-            if (curr == ')' && stack.peek() == '(') {
-                stack.pop();
-            } else
-            if (curr == ']' && stack.peek() == '[') {
-                stack.pop();
+        for(char curr: s.toCharArray()) {
+            if (curr == '(' || curr == '{' || curr == '[') {
+                if (curr == '(') {
+                    stack.push(')');
+                } else 
+                if (curr == '{') {
+                    stack.push('}');
+                } else
+                if (curr == '[') {
+                    stack.push(']');
+                }
             } else {
-                return false;
+                if (stack.isEmpty() || stack.pop() != curr) {
+                    return false;
+                }
             }
+            
         }
         return stack.isEmpty();
     }
