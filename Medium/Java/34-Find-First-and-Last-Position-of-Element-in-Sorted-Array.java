6a2e4042572b460cf6d4145cc448d262722fdb7c
Steven Godinez <steven.godin.42@gmail.com>
2026-09-24 22:59:57 -0400

    [34] [Time Beats: 100.00%] [Memory Beats: 14.36%] - LeetPush

--- Medium/Java/34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
+++ Medium/Java/34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
@@ -1,49 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = leftMostBinaryBound(nums, target);
-        int right = rightMostBinaryBound(nums, target);
+        int left = leftBinarySearch(nums, target);
+        int right = rightBinarySearch(nums, target);
         return new int[] {left, right};
     }
 
-    private int leftMostBinaryBound(int[] nums, int target) {
+
+    private int leftBinarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         int answer = -1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 answer = mid;
                 right = mid - 1;
-            } else
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else
+            }
             if (nums[mid] > target) {
                 right = mid - 1;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
             }
         }
         return answer;
     }
 
-    private int rightMostBinaryBound(int[] nums, int target) {
+    private int rightBinarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         int answer = -1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 answer = mid;
                 left = mid + 1;
-            } else
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else
+            }
             if (nums[mid] > target) {
                 right = mid - 1;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
             }
         }
         return answer;
