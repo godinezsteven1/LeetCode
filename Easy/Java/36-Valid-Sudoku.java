92612b455319cba85d260eff78215ad5f0e528ff
Steven Godinez <steven.godin.42@gmail.com>
2026-09-26 00:10:11 -0400

    [36] [Time Beats: 13.69%] [Memory Beats: 13.31%] - LeetPush

--- Easy/Java/36-Valid-Sudoku.java
+++ Easy/Java/36-Valid-Sudoku.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet<>(); 
+        HashSet<String> seen = new HashSet<>();
+
         for(int row = 0; row < 9; row++) {
             for(int col = 0; col < 9; col++) {
-                char num = board[row][col];
-                if (num != '.') {
-                    if (!seen.add(num + "row" + row)
-                    || !seen.add(num + "col" + col)
-                    || !seen.add(num + "box" + row / 3 + "-" + col / 3)) {
+                char curr = board[row][col];
+                if (curr != '.') {
+                    if (!seen.add(curr + "row" + row)
+                    || !seen.add(curr + "col" + col)
+                    || !seen.add(curr + "box" + row / 3 + "-" + col / 3)) {
                         return false;
                     }
                 }
-                
             }
         }
+
         return true;
+        
     }
 }
